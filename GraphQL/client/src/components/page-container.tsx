660f8058b21d3e1e87472377d667a1ecf20da6f5
EdonJung <2donny@naver.com>
2021-05-18 22:42:26 +0900

    schema & component

--- GraphQL/client/src/components/page-container.tsx
+++ GraphQL/client/src/components/page-container.tsx
@@ -1,20 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import styled from 'react-emotion';
-
 import { unit, colors } from '../styles';
 
 export default function PageContainer(props: any) {
   return (
-    <Fragment>
+    <>
       <Bar />
       <Container>{props.children}</Container>
-    </Fragment>
+    </>
   );
 }
-
-/**
- * STYLED COMPONENTS USED IN THIS FILE ARE BELOW HERE
- */
 
 const Bar = styled('div')({
   flexShrink: 0,
--- GraphQL/client/src/pages/launch.tsx
+++ GraphQL/client/src/pages/launch.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React from "react";
+import { RouteComponentProps } from "@reach/router";
+import { gql, useQuery } from "@apollo/client";
+import { LAUNCH_TILE_DATA } from "./launches";
+import { LaunchDetail, Header, Loading } from "../components";
+import { ActionButton } from "../containers";
+import * as LaunchDetailsTypes from "./__generated__/LaunchDetails";
+
+
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+    launch(id: $launchId) {
+      site
+      rocket {
+        type
+      }
+      ...LaunchTile
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
 
 interface LaunchProps extends RouteComponentProps {
-
+  launchId?: any;
 }
 
-const Launch: React.FC<LaunchProps> = () => {
-  return <div />;
-}
+export default function Launch({ launchId }: LaunchProps) {
+  const { 
+    data, 
+    loading, 
+    error 
+  } = useQuery<
+    LaunchDetailsTypes.LaunchDetails,
+    LaunchDetailsTypes.LaunchDetailsVariables
+  >(GET_LAUNCH_DETAILS, {
+    variables: {
+      launchId
+    }
+  });
 
-export default Launch;
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <>
+      <Header image={data.launch?.mission && data.launch.mission.missionPatch}>
+        {data && data.launch && data.launch.mission && data.launch.mission.name}
+      </Header>
+      <LaunchDetail {...data.launch} />
+      <ActionButton {...data.launch} />
+    </>
+  )
+}
